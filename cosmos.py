50eb0dfa4c289ae4cf8c024240ef8c6355ef005e
sk <satyakaligotla@users.noreply.github.com>
2022-03-17 20:04:47 -0700

    Update cosmos.py

--- cosmos.py
+++ cosmos.py
@@ -1,19 +1,19 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.providers.microsoft.azure.operators.cosmos import AzureCosmosInsertDocumentOperator
+from airflow.providers.microsoft.azure.hooks.azure_cosmos import AzureCosmosInsertDocumentOperator
 
 with DAG(
     dag_id='cosmos',
-    default_args={'database_name': 'ArchAirflowPoc'},
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    doc_md=__doc__,
     tags=['cosmos'],
 ) as dag:
 
     t1 = AzureCosmosInsertDocumentOperator(
         task_id='insert_cosmos_document',
+        database_name="ArchAirflowPoc",
         collection_name='SalesLTCustomers',
         document={"CustomerID": "1", "param1": "value1", "param2": "value2"},
+        azure_cosmos_conn_id="ARCH_AZURE_COSMOS",
     )
